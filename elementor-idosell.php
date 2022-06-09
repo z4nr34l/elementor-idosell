b73277fdd53def74a976b75169fb404a3095edfa
Mateusz Janota <software@zanreal.pl>
2022-06-09 11:53:09 +0200

    Release 2022.0.5

--- elementor-idosell.php
+++ elementor-idosell.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Elementor Idosell
 Description: Elementor integration for Idosell
-Version: 2022.0.4
+Version: 2022.0.5
 Author: Mateusz "Z4NR34L" Janota
 Author URI: https://www.zanreal.pl
 */
